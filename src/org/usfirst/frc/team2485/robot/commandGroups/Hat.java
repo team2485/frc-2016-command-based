957b87d93c11d60e8c922f7efd51daa08f7393a6
amanda-wasserman <awasserman2019@francisparker.org>
2016-08-15 21:01:04 -0700

    Created Hat command group

--- src/org/usfirst/frc/team2485/robot/commandGroups/Hat.java
+++ src/org/usfirst/frc/team2485/robot/commandGroups/Hat.java
@@ -1,7 +1,18 @@
 package org.usfirst.frc.team2485.robot.commandGroups;
 
+import org.usfirst.frc.team2485.robot.commands.IntakeArmSetManual;
+import org.usfirst.frc.team2485.robot.commands.SetBoulderStager;
+import org.usfirst.frc.team2485.robot.subsystems.BoulderStager.StagerPosition;
+
 import edu.wpi.first.wpilibj.command.CommandGroup;
 
 public class Hat extends CommandGroup {
 
+	public Hat(){
+		
+		addParallel(new SetBoulderStager(StagerPosition.SHOOTING));
+		addParallel(new IntakeArmSetManual(0.2));
+		
+	}
+	
 }
